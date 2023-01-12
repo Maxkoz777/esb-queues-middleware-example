8ac189a70d8744b621f62f9153b54ceb549d2617
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2023-01-12 15:54:11 +0300

    Initial commit

---
+++ src/test/java/com/example/esbqueuesmiddlewareexample/EsbQueuesMiddlewareExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esbqueuesmiddlewareexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbQueuesMiddlewareExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
