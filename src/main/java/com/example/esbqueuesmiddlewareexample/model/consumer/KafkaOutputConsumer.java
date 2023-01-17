de2b9c8d01dc5101c07a496d1b82591dc672c997
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2023-01-17 14:05:03 +0300

    Added KafkaOutputConsumer for consuming messages from the final queue

---
+++ src/main/java/com/example/esbqueuesmiddlewareexample/model/consumer/KafkaOutputConsumer.java
@@ -0,0 +1,51 @@
+package com.example.esbqueuesmiddlewareexample.model.consumer;
+
+
+import com.example.esbqueuesmiddlewareexample.model.messages.SimpleMessage;
+import java.util.stream.StreamSupport;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.header.Headers;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaOutputConsumer {
+
+    @KafkaListener(topics = "kafka-final-queue", clientIdPrefix = "json",
+        containerFactory = "kafkaListenerContainerFactory")
+    public void listenAsObject(ConsumerRecord<String, SimpleMessage> cr,
+                               @Payload SimpleMessage payload) {
+        log.info("Logger 1 [JSON] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                 typeIdHeader(cr.headers()), payload, cr);
+    }
+
+    @KafkaListener(topics = "kafka-final-queue", clientIdPrefix = "string",
+        containerFactory = "kafkaListenerStringContainerFactory")
+    public void listenAsString(ConsumerRecord<String, String> cr,
+                               @Payload String payload) {
+        log.info("Logger 2 [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                 typeIdHeader(cr.headers()), payload, cr);
+    }
+
+    @KafkaListener(topics = "kafka-final-queue", clientIdPrefix = "bytearray",
+        containerFactory = "kafkaListenerByteArrayContainerFactory")
+    public void listenAsByteArray(ConsumerRecord<String, byte[]> cr,
+                                  @Payload byte[] payload) {
+        log.info("Logger 3 [ByteArray] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(),
+                 typeIdHeader(cr.headers()), payload, cr);
+    }
+
+    private static String typeIdHeader(Headers headers) {
+        return StreamSupport.stream(headers.spliterator(), false)
+            .filter(header -> header.key().equals("__TypeId__"))
+            .findFirst().map(header -> new String(header.value())).orElse("N/A");
+    }
+
+
+
+}
