cbc83c2f6d57684b1edd01b849ca5078c3677c5e
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2023-01-19 16:47:51 +0300

    Added wireTap for sending message also to audit queue

--- src/main/java/com/example/esbqueuesmiddlewareexample/routes/KafkaToKafkaRoute.java
+++ src/main/java/com/example/esbqueuesmiddlewareexample/routes/KafkaToKafkaRoute.java
@@ -1,11 +1,11 @@
 package com.example.esbqueuesmiddlewareexample.routes;
 
 import com.example.esbqueuesmiddlewareexample.config.props.KafkaConfigurationProperties;
+import com.example.esbqueuesmiddlewareexample.routes.processors.BodyChangeProcessor;
 import com.example.esbqueuesmiddlewareexample.routes.processors.SimpleLoggingProcessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.kafka.KafkaConstants;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -16,16 +16,30 @@ public class KafkaToKafkaRoute extends RouteBuilder {
     private final KafkaConfigurationProperties kafkaConfigurationProperties;
     private final SimpleLoggingProcessor simpleLoggingProcessor;
 
+    private final BodyChangeProcessor bodyChangeProcessor;
+    private static final String AUDIT_TRANSACTION = "direct:audit-transaction";
+
     @Override
-    public void configure() throws Exception {
+    public void configure() {
         from(kafkaEntryQueue())
+            .wireTap(AUDIT_TRANSACTION)
             .log("Redirecting message from entry kafka queue to the output queue")
             .log("Initial message was: ${body}")
             .log("Setting message body into: Changed message by Camel Route")
             .bean(simpleLoggingProcessor)
-//            .setBody(constant("Changed message by Camel Route"))
-//            .setHeader(KafkaConstants.KEY, constant("Camel"))
+            .bean(bodyChangeProcessor)
+            .log("Updated message body after processor: ${body}")
             .to(kafkaFinalQueue());
+
+        from(AUDIT_TRANSACTION)
+            .log("Example of wire tap EIP")
+            .log("Sending message to audit queue")
+            .to(kafkaAuditQueue());
+
+        from(kafkaAuditQueue())
+            .log("Reading from audit queue")
+            .log("Message from audit queue event: ${body}");
+
     }
 
     private String kafkaEntryQueue() {
@@ -34,6 +48,10 @@ public class KafkaToKafkaRoute extends RouteBuilder {
 
     private String kafkaFinalQueue() {
         return getKafkaRouteByTopicName(kafkaConfigurationProperties.finalQueueName());
+    }
+
+    private String kafkaAuditQueue() {
+        return getKafkaRouteByTopicName(kafkaConfigurationProperties.auditQueueName());
     }
 
     private String getKafkaRouteByTopicName(String topicName) {
