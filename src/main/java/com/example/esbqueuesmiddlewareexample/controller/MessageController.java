426333f984c320602b8f4954fb8cc00fa4b543ab
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2023-01-23 11:45:34 +0300

    Added GET endpoint for printing result of REST route

--- src/main/java/com/example/esbqueuesmiddlewareexample/controller/MessageController.java
+++ src/main/java/com/example/esbqueuesmiddlewareexample/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.example.esbqueuesmiddlewareexample.controller;
 
 import com.example.esbqueuesmiddlewareexample.config.props.KafkaConfigurationProperties;
 import com.example.esbqueuesmiddlewareexample.model.messages.SimpleMessage;
+import java.security.SecureRandom;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,13 +10,14 @@ import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/messages")
+@RequestMapping("api/v1/message")
 public class MessageController {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
@@ -28,6 +30,21 @@ public class MessageController {
         kafkaTemplate.send(kafkaProperties.entryQueueName(), id, new SimpleMessage("A simple message", id));
 //        kafkaTemplate.send(kafkaProperties.entryQueueName(), id, "A simple message");
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public String getSimpleMessageWithId(@PathVariable int id) {
+        return "Got " + randomString(id);
+    }
+
+    String randomString(int len) {
+        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+
+        return sb.toString();
     }
 
 }
